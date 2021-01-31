d1c6f6bce5cac11472e6502e54dbd2f8f26c6965
Gold <proningm@gmail.com>
2021-01-31 23:05:30 +0300

    Доделываем стили футера

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,16 +1,11 @@
 let footerButtons = document.querySelectorAll('.footer__button');
-let lists = document.querySelectorAll('.footer__items');
+let list = document.querySelector('.footer__items');
 
-for (let list of lists) {
-  for (let footerButton of footerButtons) {
-    footerButton.onclick = function () {
-      footerButton.classList.toggle('footer__button-open');
-    }
-  };
-  if (footerButton.classList.contains('footer__button-open')) {
-    list.classList.remove('none');
-  } else {
-    list.classList.add('none');
-  };
+for (let footerButton of footerButtons) {
+  footerButton.onclick = function () {
+    footerButton.classList.toggle('footer__button-open');
+    list.classList.toggle('none');
+  }
 };
+
 
